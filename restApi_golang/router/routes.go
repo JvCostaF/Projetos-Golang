8de2dafe35fa389a4f07853a8f7d51345a0291e5
jvcostaf <joaovcosta220@gmail.com>
2024-11-10 11:50:02 -0300

    feat: Update Handler struct

--- restApi_golang/router/routes.go
+++ restApi_golang/router/routes.go
@@ -1,52 +1,21 @@
 package router
 
 import (
-	"net/http"
-
+	"github.com/JvCostaF/Projetos-Golang/tree/main/restApi_golang/handler"
 	"github.com/gin-gonic/gin"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/user", func(context *gin.Context) {
-			context.JSON(http.StatusOK, gin.H{ //Converte conteudo dentro do bloco em JSON
-				"name":   "Joao Victor da Costa Farias",
-				"age":    "24",
-				"status": "Single",
-			})
-		})
+		v1.GET("/user", handler.ShowUserHandler)
 
-		v1.POST("/createUser", func(context *gin.Context) {
-			context.JSON(http.StatusOK, gin.H{ //Converte conteudo dentro do bloco em JSON
-				"name":   "Joao Victor da Costa Farias",
-				"age":    "24",
-				"status": "Single",
-			})
-		})
+		v1.POST("/createUser", handler.CreateUserHandler)
 
-		v1.DELETE("/deleteUser", func(context *gin.Context) {
-			context.JSON(http.StatusOK, gin.H{ //Converte conteudo dentro do bloco em JSON
-				"name":   "Joao Victor da Costa Farias",
-				"age":    "24",
-				"status": "Single",
-			})
-		})
+		v1.DELETE("/deleteUser", handler.DeleteUserHandler)
 
-		v1.PUT("/updateUser", func(context *gin.Context) {
-			context.JSON(http.StatusOK, gin.H{ //Converte conteudo dentro do bloco em JSON
-				"name":   "Joao Victor da Costa Farias",
-				"age":    "24",
-				"status": "Single",
-			})
-		})
+		v1.PUT("/updateUser", handler.UpdateUserHandler)
 
-		v1.GET("/users", func(context *gin.Context) {
-			context.JSON(http.StatusOK, gin.H{ //Converte conteudo dentro do bloco em JSON
-				"name":   "Joao Victor da Costa Farias",
-				"age":    "24",
-				"status": "Single",
-			})
-		})
+		v1.GET("/users", handler.ListAllUsersHandler)
 	}
 }
